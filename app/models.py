f971305f4195d2cdffa6be52f7a1440291724afd
danielevans-999 <danielevans.karani@gmail.comgit>
2019-10-21 19:48:20 +0300

    Adds models, type ,pitches, and comments that are used in the application

--- app/models.py
+++ app/models.py
@@ -5,9 +5,14 @@ from werkzeug.security import generate_password_hash,check_password_hash
 class User(db.Model):
     
     __tablename__ = 'users'
+    
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique=True, index=True)
     password_hash=db.Column(db.String(255))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    pitches =  db.relationship('Pitch', backref = 'user', lazy = "dynamic")
+    Comments = db.relationship('Comment', backref = 'user', lazy = "dynamic")
     
     @property
     def password(self):
@@ -25,10 +30,65 @@ class User(db.Model):
     def __repr__(self):
         return f'User {self.username}'
     
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User', backref = 'role', lazy = "dynamic")
+
+    def __repr__(self):
+        return f'User {self.name}'
+    
+class Types(db.Model):
+    
+    __tablename__= 'types'
+    
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(255), index = True)
+    pitches = db.relationship('Pitch', backref = 'category', lazy = "dynamic")
+
+    @classmethod
+    def get_categories(cls):
+        types = Types.query.all()
+        return types
+    
+class Pitch(db.Model):
+    __tablename__ = 'pitches'
+
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(255), index = True)
+    pitch = db.Column(db.String(300), index = True)
+    time = db.Column(db.DateTime, default = datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    type_id = db.Column(db.Integer, db.ForeignKey('types.id'))
+    comments = db.relationship('Comment', backref = 'pitch', lazy = "dynamic")
+
+    def save_pitch(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_pitches(cls, id):
+        pitches = Pitch.query.filter_by(category_id = id).all()
+        return pitches
+    
+    
 class Comment(db.Model):
     
     __tablename__ = 'comments'
+    
     id = db.Column(db.Integer, primary_key = True)
     comment_post = db.Column(db.String(255), index=True)
     time = db.Column(db.DateTime, default=datetime.utcnow)
+    pitch_id = db.Column(db.Integer, db.ForeignKey('pitches.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    
+    def save_comments(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comments(cls, id):
+        comments = Comment.query.filter_by(pitch_id=id).all()
+        return comments
