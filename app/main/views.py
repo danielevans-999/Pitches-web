73f17991c391305122626f9e5313098b621fee01
danielevans-999 <danielevans.karani@gmail.comgit>
2019-10-22 00:51:43 +0300

    fixed indentation

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,7 @@
 from flask import render_template
 from . import main
+from flask_login import login_required
+from ..models import Pitch,Comment,Types
 
 @main.route('/')
 def index():
@@ -7,6 +9,26 @@ def index():
     View root page function that returns the index page and its data
     '''
     
-    return render_template('index.html')
+    types = Types.get_categories()
     
+    title = 'Pitch up your Pitch'
+    return render_template('index.html',types=types, title=title)
     
+@main.route('/type/<int:id>')
+def single_type(id):
+    '''
+    A view function that will return the pitches on a specific kind of view
+    '''
+
+    types = Types.query.get(id)
+    title = f'{types.name} pitches'
+    pitches = Pitch.get_pitches(types.id)
+
+    return render_template('type.html', title=title,types=types,pitches=pitches)
+
+@main.route('/type/pitch/new/<int : id>', methods=["GET","POST"])
+@login_required
+def pitch_new(id):
+    '''
+    view function that helps renders theform to create a new pitch
+    '''
