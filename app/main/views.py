18116076e2655fda4bd4a7eaaeb4dcaa1a69fa90
danielevans-999 <danielevans.karani@gmail.comgit>
2019-10-22 05:10:39 +0300

    Addsroutes for updating the biodata and the profile picture

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,9 @@
-from flask import render_template, redirect, url_for,abort
+from flask import render_template,request, redirect, url_for,abort
 from . import main
 from flask_login import login_required, current_user
 from ..models import Pitch, Comment, Types,User
 from .forms import PitchForm, CommentsForm,UpdateProfile
-from ..import db
+from ..import db,photos
 
 
 @main.route('/')
@@ -107,3 +107,16 @@ def update_profile(uname):
         return redirect(url_for('.profile',uname=user.username))
 
     return render_template('profile/update.html',form =form)
+
+
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
