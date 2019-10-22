73b7a2d30251d3bdcac5d04bb60c699c3cb5f61c
danielevans-999 <danielevans.karani@gmail.comgit>
2019-10-22 05:19:03 +0300

    Adds the flask_email extension initialisation

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,11 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
 
 bootstrap = Bootstrap()
 db=SQLAlchemy()
+mail = Mail()
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -30,6 +32,7 @@ def create_app(config_name):
     # Initializing Flask Extensions
     bootstrap.init_app(app)
     db.init_app(app)
+    mail.init_app(app)
     
     # configure UploadSet
     configure_uploads(app,photos)
