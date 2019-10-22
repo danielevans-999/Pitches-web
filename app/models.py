8dc742884cfbdf2c5932b762a38079d0eec0452b
danielevans-999 <danielevans.karani@gmail.comgit>
2019-10-22 04:53:56 +0300

    Update the files indentations

--- app/models.py
+++ app/models.py
@@ -1,8 +1,15 @@
-from . import db
+from . import db,login_manager
 from datetime import datetime
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login  import UserMixin
 
-class User(db.Model):
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(UserMixin,db.Model):
     
     __tablename__ = 'users'
     
@@ -11,6 +18,8 @@ class User(db.Model):
     email = db.Column(db.String(255), unique=True, index=True)
     password_hash=db.Column(db.String(255))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
     pitches =  db.relationship('Pitch', backref = 'user', lazy = "dynamic")
     Comments = db.relationship('Comment', backref = 'user', lazy = "dynamic")
     
