e5a8448e90df9b2e845567c9e7c9d1cca4a9dd9f
danielevans-999 <danielevans.karani@gmail.comgit>
2019-10-23 23:46:02 +0300

    Changes to production environment

--- config.py
+++ config.py
@@ -8,6 +8,9 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
     
 class ProdConfig(Config):
     '''
